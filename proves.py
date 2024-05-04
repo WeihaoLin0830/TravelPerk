70fbb6446ad72ff24b6601655e7ef998df752e6c
WeihaoLin0830 <weihaolin0830@gmail.com>
2024-05-04 17:40:22 +0200

    majoria arrglat

--- proves.py
+++ proves.py
@@ -2,7 +2,7 @@ import pandas as pd
 import random
 
 # Replace 'path_to_csv_file' with the actual path to your CSV file
-file_path = 'C:/Users/weiha/Documents/UPC/Programación/hackupc/hackupc-travelperk-dataset.csv'
+file_path = 'C:/Users/weiha/Documents/UPC/Programación/hackupc/hackupc-travelperk-dataset-extended.csv'
 
 # Read the CSV file into a pandas DataFrame
 df = pd.read_csv(file_path)
@@ -24,8 +24,8 @@ llista_de_gustos = ["Cultura","Aire lliure","Història","Compres","Ciència","Fa
     df.iloc[i,7] = presupost
     df.to_csv(file_path, index=False)"""
     
-llista = [43, 140, 177, 245, 279, 408, 442, 488, 510, 515, 630, 665, 685, 828, 987, 43, 111, 132, 133, 140, 177, 279, 360, 442, 510, 515, 622, 630, 685, 768, 807, 987, 43, 111, 132, 177, 245, 279, 360, 442, 510, 622, 665, 685, 768, 828, 43, 140, 160, 177, 279, 408, 442, 488, 515, 630, 685, 768, 807, 987, 132, 133, 140, 160, 360, 442, 488, 510, 515, 630, 685, 768, 807, 111, 132, 133, 140, 160, 245, 408, 442, 488, 510, 622, 630, 665, 685, 768, 807, 828, 987]
+"""llista = [43, 140, 177, 245, 279, 408, 442, 488, 510, 515, 630, 665, 685, 828, 987, 43, 111, 132, 133, 140, 177, 279, 360, 442, 510, 515, 622, 630, 685, 768, 807, 987, 43, 111, 132, 177, 245, 279, 360, 442, 510, 622, 665, 685, 768, 828, 43, 140, 160, 177, 279, 408, 442, 488, 515, 630, 685, 768, 807, 987, 132, 133, 140, 160, 360, 442, 488, 510, 515, 630, 685, 768, 807, 111, 132, 133, 140, 160, 245, 408, 442, 488, 510, 622, 630, 665, 685, 768, 807, 828, 987]
 
 no_reps = len(set(llista))
 
-print(no_reps)
+print(no_reps)"""
