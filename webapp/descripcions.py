d0df542ffcacba3b5d95a2a00643ada184682037
WeihaoLin0830 <weihaolin0830@gmail.com>
2024-05-04 13:28:52 +0200

    ?

--- webapp/descripcions.py
+++ webapp/descripcions.py
@@ -1,14 +1,13 @@
 import openai
-from openai import OpenAI
 
-openai.api_key = 'sk-proj-Ym9QzZuk5v9OUDJXBz5rT3B1bkFJ4oLG5Ao'
-client = OpenAI()
+openai.api_key = 'sk-proj-kFIaBVsdYPM9VKtlLC8sT3BlbkFJRahplEZyfzb35x0Q5LPH'
+
 lloc = "Londres"
 
 text = "I want a json of places to visit in "+lloc+" 'name', 'short description', 'long description','budget in euros'(value) 'type of plan ([culture,history,science,familiar,relax,party,shopping,outdoors activities])'"
 
-response = client.chat.completions.create(
-  model="gpt-4",
+response = openai.ChatCompletion.create(
+  model="gpt-3.5-turbo",
   response_format={ "type": "json_object" },
   messages=[
     {"role": "system", "content": "You are a helpful assistant designed to output JSON."},
