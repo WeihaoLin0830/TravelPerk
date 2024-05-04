b7e03ad497af29a8f3480f576cbc08378903ab8c
claudia <claudia.gallego@estudiantat.upc.edu>
2024-05-04 20:44:25 +0200

    final? llista pliiiis

--- llista_temp.py
+++ llista_temp.py
@@ -14,38 +14,78 @@ def llista_coincidencies(trip_id):
         desti2 = df.iloc[other-1,5]
 
         if sortida1 == sortida2:
-            return False
+            return False, [] 
         else:
             if max(start1, start2) <= min(end1, end2):
                 if desti1==desti2:
-                    return True
+                    return True, [max(start1, start2),min(end1, end2)]
                 else:
                     if desti1==sortida2:
                         if start1 < start2 or end1 > end2:
-                            return True
+                            return True, [max(start1, start2),min(end1, end2)]
                         else:
-                            return False
+                            return False, [] 
 
                     elif desti2==sortida1:
                         if start1 > start2 or end1 < end2:
-                            return True
+                            return True, [max(start1, start2),min(end1, end2)]
                         else:
-                            return False
+                            return False, [] 
                            
                     else:
-                        return False  
+                        return False, [] 
                     
             else:
-                if sortida1==desti2 or desti1==sortida2:
-                    return True
+                if sortida1==desti2:
+                    return True, [start2, end2]
+                
+                elif desti1==sortida2:
+                    return True, [start1,end1]
+                
                 else:
-                    return False
-
+                    return False, []  
+    
     llista = []
+    llista_data = {}
 
     for i in range(1,len(df)+1):
         if i != trip_id:
-            if coincidencia(i):
-                llista.append(df.iloc[i-1,0])
 
-    return llista
+            indicador, data = coincidencia(i)
+
+            if indicador:
+                llista.append(df.iloc[i-1,0])
+                llista_data[i-1]=data
+
+    return llista, llista_data
+
+def gustos(trip_id):
+    coin = {}
+    llista, llista_data = llista_coincidencies(trip_id)
+    
+    for gustos in eval(df.iloc[trip_id-1,6]):
+        for j in llista:
+            
+            numero_conincidencias = 0
+            
+            if gustos in eval(df.iloc[j-1,6]):
+                for i in range(len(eval(df.iloc[trip_id-1,6]))):
+                    for k in range(len(eval(df.iloc[j-1,6]))):
+                        if eval(df.iloc[trip_id-1,6])[i] == eval(df.iloc[j-1,6])[k]:
+                            numero_conincidencias += 1
+                
+                coin[j] = numero_conincidencias
+    
+    sort_coin = dict(sorted(coin.items(), key=lambda x: x[1], reverse=True))
+    
+    return sort_coin, llista_data
+
+
+def coincidencias_orden(trip_id):
+
+    llista, llista_data = gustos(trip_id)
+
+    presu = {key: [df.iloc[key-1,1],value, df.iloc[key-1,7], llista_data[key-1]] for key, value, in llista.items()}
+    sorted_presu = dict(sorted(presu.items(), key=lambda item: (item[1][1], -abs(item[1][2] - df.iloc[trip_id-1,7])), reverse=True))
+    return sorted_presu
+
