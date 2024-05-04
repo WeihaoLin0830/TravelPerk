8846695173cb3412c8a582d044feaa4205d0aa6e
Stargix <11sergifm@gmail.com>
2024-05-04 13:02:57 +0200

    chatgpt

--- webapp/descripcions.py
+++ webapp/descripcions.py
@@ -1,1 +1,18 @@
+from openai import OpenAI
 
+openai.api_key = 'sk-proj-Ym9QzZuk5v9OUDJXBz5rT3B1bkFJ4oLG5Ao'
+client = OpenAI()
+lloc = "Londres"
+response = client.chat.completions.create(
+  model="gpt-4",
+  response_format={ "type": "json_object" },
+  messages=[
+    {"role": "system", "content": "You are a helpful assistant designed to output JSON."},
+    {"role": "user", "content": f"I want a json of places to visit in {lloc}.
+     'name',
+     'short description'
+     'long description',
+     'budget in euros':(value),
+     'type of plan ([culture,history,science,familiar,relax,party,shopping,outdoors activities])'"}]
+)
+print(response.choices[0].message.content)
