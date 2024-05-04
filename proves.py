2e9f0bcd63ec2212d3a1ea9a6baa8372a1a33442
claudia <claudia.gallego@estudiantat.upc.edu>
2024-05-04 09:32:26 +0200

    presupost

--- proves.py
+++ proves.py
@@ -19,4 +19,9 @@ llista_de_gustos = ["Cultura","Aire lliure","Història","Compres","Ciència","Fa
     df.iloc[i,6] = str(likes)
     df.to_csv(file_path, index=False)"""
 
+for i in range(len(df)):
+    gust_rand = random.choice(range(0,250))
+    df.iloc[i,7] = gust_rand
+    df.to_csv(file_path, index=False)
+
 print(eval(df.iloc[6,6]))
