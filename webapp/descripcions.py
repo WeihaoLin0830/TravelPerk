18df6207a4119a8ef0de2c41ae8edbffb7731173
WeihaoLin0830 <weihaolin0830@gmail.com>
2024-05-04 13:54:58 +0200

    llama3

--- webapp/descripcions.py
+++ webapp/descripcions.py
@@ -1,16 +1,18 @@
-import openai
+from groq import Groq
 
-openai.api_key = 'sk-proj-kFIaBVsdYPM9VKtlLC8sT3BlbkFJRahplEZyfzb35x0Q5LPH'
+client = Groq(
+    api_key="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",
+)
 
 lloc = "Londres"
-
 text = "I want a json of places to visit in "+lloc+" 'name', 'short description', 'long description','budget in euros'(value) 'type of plan ([culture,history,science,familiar,relax,party,shopping,outdoors activities])'"
 
-response = openai.ChatCompletion.create(
-  model="gpt-3.5-turbo",
-  response_format={ "type": "json_object" },
-  messages=[
+chat_completion = client.chat.completions.create(
+    messages=[
     {"role": "system", "content": "You are a helpful assistant designed to output JSON."},
-    {"role": "user", "content": text}]
+    {"role": "user", "content": text}
+    ],
+    model="llama3-70b-8192",
 )
-print(response.choices[0].message.content)
+
+print(chat_completion.choices[0].message.content)
